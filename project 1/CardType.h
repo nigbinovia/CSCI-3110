a4d4ceac69884916852527f6db2e60572236c31f
Naomi Igbinovia <97126858+nigbinovia@users.noreply.github.com>
2023-08-28 10:28:54 -0500

    Update CardType.h

--- project 1/CardType.h
+++ project 1/CardType.h
@@ -1,1 +1,11 @@
+// The enum type for card suits are defined 
+enum CardSuitType {DIAMOND, CLUB, HEART, SPADE};
 
+
+// A struct is defined with suit, value, and point members is defined 
+ struct CardType
+  {
+    CardSuitType suit;
+    int value;
+    int points;
+  };
